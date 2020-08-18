be52694e4a1c3281a8471677ca51585b4dce4d38
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2020-08-18 17:35:30 -0400

    add all product cards to the shop menu.

--- script.js
+++ script.js
@@ -45,7 +45,17 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
-//Shop Menu Functions
+// ------- Shop Menu Functions
+
+// Shop Menu Event Listeners
+
+document.getElementById('item_1_switch').addEventListener('mouseenter', function() {
+  this.src = 'img/apparel/shop_item_1_alt.jpg';
+});
+
+document.getElementById('item_1_switch').addEventListener('mouseleave', function() {
+  this.src = 'img/apparel/shop_item_1.jpg';
+});
 
 document.getElementById('item_3_switch').addEventListener('mouseenter', function() {
   this.src = 'img/apparel/shop_item_3_alt.png';
@@ -54,3 +64,58 @@ document.getElementById('item_3_switch').addEventListener('mouseenter', function
 document.getElementById('item_3_switch').addEventListener('mouseleave', function() {
   this.src = 'img/apparel/shop_item_3.png';
 });
+
+document.getElementById('item_7_switch').addEventListener('mouseenter', function() {
+  this.src = 'img/apparel/shop_item_7_alt.png';
+});
+
+document.getElementById('item_7_switch').addEventListener('mouseleave', function() {
+  this.src = 'img/apparel/shop_item_7.png';
+});
+
+document.getElementById('item_8_switch').addEventListener('mouseenter', function() {
+  this.src = 'img/apparel/shop_item_8_alt.png';
+});
+
+document.getElementById('item_8_switch').addEventListener('mouseleave', function() {
+  this.src = 'img/apparel/shop_item_8.png';
+});
+
+document.getElementById('item_9_switch').addEventListener('mouseenter', function() {
+  this.src = 'img/apparel/shop_item_9_alt.png';
+});
+
+document.getElementById('item_9_switch').addEventListener('mouseleave', function() {
+  this.src = 'img/apparel/shop_item_9.png';
+});
+
+document.getElementById('item_10_switch').addEventListener('mouseenter', function() {
+  this.src = 'img/apparel/shop_item_10_alt.png';
+});
+
+document.getElementById('item_10_switch').addEventListener('mouseleave', function() {
+  this.src = 'img/apparel/shop_item_10.png';
+});
+
+// Shop Page Dropdown Menu
+
+document.getElementById("shop_item_4_switch").onchange = changeShopItemFour;
+
+function changeShopItemFour() {
+  var img = document.getElementById("item_4_switch");
+  img.src = this.value;
+}
+
+function flipShopItemFour() {
+
+  var x = document.getElementById('item_4_switch');
+
+  if (x.src === "img/apparel/shop_item_4.png") {
+    x.src = "img/apparel/shop_item_4_alt.png";
+  } else if (x.src === "img/apparel/shop_item_4_alt_1.png") {
+    x.src = "img/apparel/shop_item_4_alt_2.png";
+  } else if (x.src === "img/apparel/shop_item_4_alt_3.png") {
+    x.src = "img/apparel/shop_item_4_alt_4.png"
+  }
+  
+}
