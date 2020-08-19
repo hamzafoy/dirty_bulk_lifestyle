3b9653e192b1f9ac722460f6c299328d8ef2a281
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2020-08-19 15:30:07 -0400

    will the rest of the functionality to the product cards in the shop.html page.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Navigation Menu JS
 
+
+
 function menuFunction() {
     var x = document.getElementById("myLinks");
     var v = document.getElementById("shop_submenu");
@@ -45,9 +47,15 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
+
+
 // ------- Shop Menu Functions
 
+
+
 // Shop Menu Event Listeners
+
+
 
 document.getElementById('item_1_switch').addEventListener('mouseenter', function() {
   this.src = 'img/apparel/shop_item_1_alt.jpg';
@@ -97,7 +105,11 @@ document.getElementById('item_10_switch').addEventListener('mouseleave', functio
   this.src = 'img/apparel/shop_item_10.png';
 });
 
+
+
 // Shop Page Dropdown Menu
+
+
 
 document.getElementById("shop_item_4_switch").onchange = changeShopItemFour;
 
@@ -107,7 +119,7 @@ function changeShopItemFour() {
 }
 
 document.getElementById('item_4_switch').addEventListener('mouseenter', function() {
-  
+
   var x = document.getElementById('shop_item_4_switch')
 
   if (x.value === "img/apparel/shop_item_4.png") {
@@ -117,7 +129,7 @@ document.getElementById('item_4_switch').addEventListener('mouseenter', function
   } else if (x.value === "img/apparel/shop_item_4_alt_3.png") {
     this.src = "img/apparel/shop_item_4_alt_4.png"
   }
-  
+
 });
 
 document.getElementById('item_4_switch').addEventListener('mouseleave', function() {
@@ -130,6 +142,78 @@ document.getElementById('item_4_switch').addEventListener('mouseleave', function
     this.src = "img/apparel/shop_item_4_alt_1.png";
   } else if (x.value === "img/apparel/shop_item_4_alt_3.png") {
     this.src = "img/apparel/shop_item_4_alt_3.png"
+  }
+
+});
+
+
+document.getElementById("shop_item_5_switch").onchange = changeShopItemFive;
+
+function changeShopItemFive() {
+  var img = document.getElementById("item_5_switch");
+  img.src = this.value;
+}
+
+document.getElementById('item_5_switch').addEventListener('mouseenter', function() {
+
+  var x = document.getElementById('shop_item_5_switch')
+
+  if (x.value === "img/apparel/shop_item_5.png") {
+    this.src = "img/apparel/shop_item_5_alt.png";
+  } else if (x.value === "img/apparel/shop_item_5_alt_1.png") {
+    this.src = "img/apparel/shop_item_5_alt_2.png";
+  } else if (x.value === "img/apparel/shop_item_5_alt_3.png") {
+    this.src = "img/apparel/shop_item_5_alt_4.png"
+  } else if (x.value === "img/apparel/shop_item_5_alt_5.png") {
+    this.src = "img/apparel/shop_item_5_alt_6.png"
+  }
+  
+});
+
+document.getElementById('item_5_switch').addEventListener('mouseleave', function() {
+  
+  var x = document.getElementById('shop_item_5_switch')
+
+  if (x.value === "img/apparel/shop_item_5.png") {
+    this.src = "img/apparel/shop_item_5.png";
+  } else if (x.value === "img/apparel/shop_item_5_alt_1.png") {
+    this.src = "img/apparel/shop_item_5_alt_1.png";
+  } else if (x.value === "img/apparel/shop_item_5_alt_3.png") {
+    this.src = "img/apparel/shop_item_5_alt_3.png"
+  } else if (x.value === "img/apparel/shop_item_5_alt_5.png") {
+    this.src = "img/apparel/shop_item_5_alt_5.png"
+  }
+  
+});
+
+
+document.getElementById("shop_item_11_switch").onchange = changeShopItemFive;
+
+function changeShopItemFive() {
+  var img = document.getElementById("item_11_switch");
+  img.src = this.value;
+}
+
+document.getElementById('item_11_switch').addEventListener('mouseenter', function() {
+
+  var x = document.getElementById('shop_item_11_switch')
+
+  if (x.value === "img/apparel/shop_item_11.png") {
+    this.src = "img/apparel/shop_item_11_alt.png";
+  } else if (x.value === "img/apparel/shop_item_11_alt_1.png") {
+    this.src = "img/apparel/shop_item_11_alt_2.png";
+  }
+  
+});
+
+document.getElementById('item_11_switch').addEventListener('mouseleave', function() {
+  
+  var x = document.getElementById('shop_item_11_switch')
+
+  if (x.value === "img/apparel/shop_item_11.png") {
+    this.src = "img/apparel/shop_item_11.png";
+  } else if (x.value === "img/apparel/shop_item_11_alt_1.png") {
+    this.src = "img/apparel/shop_item_11_alt_1.png";
   }
   
 });
